d473b741f5ba099def880e246805dd29c260149e
Mike Reining <emailmike@gmail.com>
2015-01-29 14:30:35 -0800

    Starting to work with custom flow layouts

---
+++ InstaKilo/CustomCollectionViewFlowLayout.swift
@@ -0,0 +1,13 @@
+//
+//  CustomCollectionViewFlowLayout.swift
+//  InstaKilo
+//
+//  Created by Michael Reining on 1/29/15.
+//  Copyright (c) 2015 Mike Reining. All rights reserved.
+//
+
+import UIKit
+
+class CustomCollectionViewFlowLayout: UICollectionViewFlowLayout {
+   
+}
