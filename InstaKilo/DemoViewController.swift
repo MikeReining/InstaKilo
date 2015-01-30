5e7452a63018125a6553a145c7373cc11303a13a
Mike Reining <emailmike@gmail.com>
2015-01-30 06:35:31 -0800

    Added alert message to segmented control

--- InstaKilo/DemoViewController.swift
+++ InstaKilo/DemoViewController.swift
@@ -10,10 +10,10 @@ import UIKit
 
 class DemoViewController: UIViewController {
     @IBAction func layoutSegmentChanged(sender: UISegmentedControl) {
-//        let cvc = CollectionViewController()
-//        let layout = CollectionViewSmallFlowLayout()
-//        cvc.smallLayout .invalidateLayout()
-//        cvc.collectionView?.setCollectionViewLayout(layout, animated: true)
+        let cvc = CollectionViewController()
+        let smallLayout = CollectionViewSmallFlowLayout()
+        cvc.smallLayout.invalidateLayout()
+        cvc.collectionView?.setCollectionViewLayout(smallLayout, animated: false)
         let alert = UIAlertController(title: "Bummer!", message: "Switching between BIG and SMALL layouts only works if you change the custom class for the Collection View in the storyboard.", preferredStyle: .Alert)
         let action = UIAlertAction(title: "Please help", style: .Default, handler: nil)
         alert.addAction(action)
